baae33dcac75d32d5bc18797b9225bd0e04b21f7
Masafumi Yokoyama <myokoym@gmail.com>
2012-10-18 01:04:08 +0900

    Implement increment option

--- spec/texttexttext_spec.rb
+++ spec/texttexttext_spec.rb
@@ -33,6 +33,18 @@ describe "TextTextText" do
       last_response.ok? == true
       last_response.body.to_s.should =~ />text&#x000A;text&#x000A;text<\/textarea>/
     end
+
+    it "increment option" do
+      post '/clone', {:text => "text_#;",
+                      :times => "3",
+                      :linefeed => "0",
+                      :increment => "1",
+                      :increment_sign => "#",
+                      :increment_start => "1",
+                      :increment_digit => "3"}
+      last_response.ok? == true
+      last_response.body.to_s.should =~ />text_001;text_002;text_003;<\/textarea>/
+    end
   end
 end
 
--- texttexttext.rb
+++ texttexttext.rb
@@ -19,6 +19,20 @@ end
 private
 def clone(params)
   text = params[:text]
+  if params[:increment] == "1"
+    result = ""
+    format = "%0#{params[:increment_digit]}d"
+    sign = params[:increment_sign]
+    sign_count = text.count(sign)
+    text.gsub!(/#{sign}/, format)
+    start = params[:increment_start].to_i
+    params[:times].to_i.times do |i|
+      result << text % Array.new(sign_count) { i + start }
+      result << "\n" if params[:linefeed] == "1"
+    end
+    result
+  else
   text << "\n" if params[:linefeed] == "1"
   text * params[:times].to_i
+  end
 end
