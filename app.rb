8fda004ba54f8bf88783c2643fab659dc91acf2d
Masafumi Yokoyama <myokoym@gmail.com>
2019-11-09 00:06:54 +0900

    Import old (Sinatra x Haml) version

--- app.rb
+++ app.rb
@@ -1,43 +1,111 @@
 require 'ovto'
 
-class MyApp < Ovto::App
-  class State < Ovto::State
-    item :celsius, default: 0
+require "components/body"
 
-    def fahrenheit
-      (celsius * 9 / 5.0) + 32
+class Textamp < Ovto::App
+  class State < Ovto::State
+    item :text, default: "aaa#"
+    item :volume, default: 10
+    item :increment, default: true
+    item :increment_digit, default: 3
+    item :increment_sign, default: "#"
+    item :increment_start, default: 0
+    item :template, default: false
+    item :template_sign, default: ""
+    item :template_text, default: ""
+    item :linefeed, default: true
+
+    def result
+      results = Array.new(volume) { text }
+
+      if increment
+        results = do_increment(results)
+      end
+
+      if template
+        results = do_template(results)
+      end
+
+      result = ""
+      if linefeed
+        result = results.join("\n")
+      else
+        result = results.join(" ")
+      end
+
+      result
+    end
+
+    private
+
+    def do_increment(results)
+      format = "%0#{increment_digit}d"
+      sign = increment_sign
+      start = increment_start
+      results.map.with_index do |t, i|
+        sign_count = t.count(sign)
+        part = format % (i + start)
+        t.gsub("#{sign}", part)
+      end
+    end
+
+    def do_template(results)
+      sign = template_sign
+      results.map.with_index do |t, i|
+        template_texts = template_text.split(/\n/)
+        part = template_texts[i % template_texts.count].chomp
+        t.gsub("#{sign}", part)
+      end
     end
   end
 
   class Actions < Ovto::Actions
-    def set_celsius(value:)
-      return {celsius: value}
+    def set_text(value: value)
+      return {text: value}
     end
 
-    def set_fahrenheit(value:)
-      new_celsius = (value - 32) * 5 / 9.0
-      return {celsius: new_celsius}
+    def set_volume(value: value)
+      return {volume: value.to_i}
+    end
+
+    def set_increment(value: value)
+      return {increment: value}
+    end
+
+    def set_increment_digit(value: value)
+      return {increment_digit: value.to_i}
+    end
+
+    def set_increment_sign(value: value)
+      return {increment_sign: value}
+    end
+
+    def set_increment_start(value: value)
+      return {increment_start: value.to_i}
+    end
+
+    def set_template(value: value)
+      return {template: value.to_i > 0}
+    end
+
+    def set_template_sign(value: value)
+      return {template_sign: value}
+    end
+
+    def set_template_text(value: value)
+      return {template_text: value}
+    end
+
+    def set_linefeed(value: value)
+      return {linefeed: value}
     end
   end
 
   class MainComponent < Ovto::Component
     def render
-      o 'div' do
-        o 'span', 'Celcius:'
-        o 'input', {
-          type: 'text',
-          onchange: ->(e){ actions.set_celsius(value: e.target.value.to_i) },
-          value: state.celsius
-        }
-        o 'span', 'Fahrenheit:'
-        o 'input', {
-          type: 'text',
-          onchange: ->(e){ actions.set_fahrenheit(value: e.target.value.to_i) },
-          value: state.fahrenheit
-        }
-      end
+      o Body
     end
   end
 end
 
-MyApp.run(id: 'ovto')
+Textamp.run(id: 'ovto')
---
+++ components/body.rb
@@ -0,0 +1,120 @@
+class Textamp < Ovto::App
+  class Body < Ovto::Component
+    def render
+      o "div" do
+        o "h1", "Textamp"
+
+        o "p" do
+          o "span", "volume"
+          o "select", {
+            name: "volume",
+            onchange: ->(e){ actions.set_volume(value: e.target.value) },
+          } do
+            1.upto(100) do |i|
+              hash = {value: i}
+              hash[:selected] = "selected" if i == state.volume
+              o "option", hash, i
+            end
+          end
+          o "span", "　"
+          hash = {
+            type: "checkbox",
+            name: "linefeed",
+            onchange: ->(e){ actions.set_linefeed(value: e.target.checked) },
+          }
+          hash[:checked] = "checked" if state.linefeed
+          o "input", hash
+          o "span", "line feed"
+        end
+
+        o "p" do
+          hash = {
+            type: "checkbox",
+            name: "increment",
+            onchange: ->(e){ actions.set_increment(value: e.target.checked) },
+          }
+          hash[:checked] = "checked" if state.increment
+          o "input", hash
+          o "span", "increment"
+          o "span", "　"
+          o "span", "sign"
+          o "select", {
+            name: "increment_sign",
+            onchange: ->(e){ actions.set_increment_sign(value: e.target.value) },
+          } do
+            %w(# $ o " & @ + * ? _ -).each do |sign|
+              hash = {value: sign}
+              hash[:selected] = "selected" if sign == state.increment_sign
+              o "option", hash, sign
+            end
+          end
+          o "span", "　"
+          o "span", "start"
+          o "select", {
+            name: "increment_start",
+            onchange: ->(e){ actions.set_increment_start(value: e.target.value) },
+          } do
+            [0, 1].each do |i|
+              hash = {value: i}
+              hash[:selected] = "selected" if i == state.increment_start.to_i
+              o "option", hash, i
+            end
+          end
+          o "span", "　"
+          o "span", "digit"
+          o "select", {
+            name: "increment_digit",
+            onchange: ->(e){ actions.set_increment_digit(value: e.target.value) },
+          } do
+            1.upto(10).each do |i|
+              hash = {value: i}
+              hash[:selected] = "selected" if i == state.increment_digit.to_i
+              o "option", hash, i
+            end
+          end
+        end
+
+        o "p" do
+          hash = {
+            type: "checkbox",
+            name: "template",
+            onchange: ->(e){ actions.set_template(value: e.target.checked) },
+          }
+          hash[:checked] = "checked" if state.template
+          o "input", hash
+          o "span", "template"
+          o "span", "　"
+          o "span", "sign"
+          o "input", {
+            type: "text",
+            name: "template_sign",
+            size: 5,
+            value: state.template_sign,
+            oninput: ->(e){ actions.set_template_sign(value: e.target.value) },
+          }
+          o "span", "　"
+          o "span", "parts"
+          o "textarea", {
+            name: "template_text",
+            cols: 20,
+            rows: 1,
+            oninput: ->(e){ actions.set_template_text(value: e.target.value) },
+          }, state.template_text
+        end
+
+        o "p" do
+          o "textarea", {
+            name: "text",
+            cols: 40,
+            rows: 5,
+            oninput: ->(e){ actions.set_text(value: e.target.value) },
+          }, state.text
+        end
+
+        o "p" do
+          o "textarea", {:cols => 40, :rows => 10}, state.result
+        end
+      end
+    end
+  end
+end
